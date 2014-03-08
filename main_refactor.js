fc75fdc0416a2995c8042b6ab626d89aa58ea655
Vincent Schramer <vinciple@gmail.com>
2014-03-08 13:22:48 -0600

    Removing old backend in favor of refactored version.

--- main_refactor.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var config = require('./config.js');
-
-var webJCLExpressApp = require('./module-config/WebJCLExpressApp');
-
-// Start HTTP server
-webJCLExpressApp.listen(config.httpPort);
